77db1526ea13d91681715df2ef7803ccc7810788
Rillian Diello Lucas Pires <rillianpires@sescms.com.br>
2020-05-14 14:02:43 -0400

    base das classes

---
+++ processamento_contratos/Entities/Installment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace processamento_contratos.Services
+{
+    class Installment
+    {
+
+        public DateTime dueDate { get; set; }
+
+        public double amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            this.dueDate = dueDate;
+            this.amount = amount;
+        }
+    }
+}
