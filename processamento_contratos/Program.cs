5dd38d077ca1e88cf66fadd0e266cf671fc94456
Rillian Diello Lucas Pires <rillianpires@sescms.com.br>
2020-05-14 13:08:45 -0400

    criacao do projeto

---
+++ processamento_contratos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace processamento_contratos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
